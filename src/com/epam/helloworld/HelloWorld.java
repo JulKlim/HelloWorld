6177ca92ac7f16bb09f635e3430c2e8924a18c7d
JulKlim <jklimmov@gmail.com>
2023-03-10 16:38:07 +0300

    [6788] Changing to "Friends"

--- src/com/epam/helloworld/HelloWorld.java
+++ src/com/epam/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package com.epam.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.printf("Hello %s!", args[0]);
     }
 }
