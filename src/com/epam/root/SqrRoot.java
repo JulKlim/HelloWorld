0c3dfb8a66adbd2a9d5bb5758e79192568a0b692
JulKlim <jklimmov@gmail.com>
2023-03-24 12:50:13 +0300

    [6788] Search for sqr root is added

--- src/com/epam/root/SqrRoot.java
+++ src/com/epam/root/SqrRoot.java
@@ -2,7 +2,7 @@ package com.epam.root;
 
 public class SqrRoot {
     public static void main(String[] args) {
-        int x = 10;
+        int x = 16;
         int n = x;
         while (n*n > x)
             n = (n + x / n) / 2;
