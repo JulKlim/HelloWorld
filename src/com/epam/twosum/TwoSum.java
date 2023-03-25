16148eac2d2c2ca4e9eadb3a74b0eeeef5ab69dc
JulKlim <jklimmov@gmail.com>
2023-03-25 17:31:50 +0300

    TwoSum solution is updated

--- src/com/epam/twosum/TwoSum.java
+++ src/com/epam/twosum/TwoSum.java
@@ -1,22 +1,27 @@
 package com.epam.twosum;
-
-public class TwoSum {
-
-    public static void main(String[] args) {
-
-        int[] nums = {1, 2, 4, 5};
-        int target = 9;
+import java.util.Arrays;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int n = i + 1; n < nums.length; n++) {
                 int number = nums[i] + nums[n];
                 int[] twonums = new int[2];
-                twonums[0]=i;
-                twonums[1]=n;
+                twonums[0] = i;
+                twonums[1] = n;
                 if (number == target) {
-                    System.out.println(java.util.Arrays.toString(twonums));
+                    return twonums;
                 }
             }
         }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[] {1,2,4,5,8,3};
+        int target = 10;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
     }
 }
 
